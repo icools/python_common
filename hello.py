488ef4e35937bf4586f927a1cf4685da442608b3
Jeff <icools.tw@gmail.com>
2024-09-20 18:22:02 +0000

    Refactor hello.py and samba_nova.py

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-# print hello world and 
+from typing import Final
+from samba_nova import chat_with_samba_nova
 
-print("hello")
+def chat(message):
+    return chat_with_samba_nova(message)
+
+content: Final = chat("到底是公蝦小還是母蝦小").choices[0].message.content
+
+print(content)
---
+++ samba_nova.py
@@ -0,0 +1,23 @@
+# samba_nova.py
+
+import openai
+import os
+
+# @title SambaNova
+def chat_with_samba_nova(message):
+    SAMBANOVA_API_KEY = os.getenv("SAMBA_NOVA_KEY")
+    model = "Meta-Llama-3.1-8B-Instruct" # @param ["Meta-Llama-3.1-8B-Instruct","Meta-Llama-3.1-70B-Instruct","Meta-Llama-3.1-405B-Instruct"]
+    client = openai.OpenAI(
+        api_key = SAMBANOVA_API_KEY,
+        base_url = "https://api.sambanova.ai/v1",
+    )
+
+    return client.chat.completions.create(
+        model = model,
+        messages=[
+            {"role":"system","content":"You are a helpful assistant"},
+            {"role":"user","content":"台天氣如何"}
+        ],
+        temperature =  0.1,
+        top_p = 0.1
+    )

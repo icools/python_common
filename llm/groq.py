5bde01c26b4197f45a95e818de10c285c264d862
Jeff <icools.tw@gmail.com>
2024-09-21 09:42:56 +0000

    Refactor requirements.txt and add groq and google-generativeai dependencies

---
+++ llm/groq.py
@@ -0,0 +1,21 @@
+from groq import Groq
+import os
+
+class GroqApi:
+    def __init__(self):
+        self.api_key = os.getenv("GROQ_KEY")
+        self.client = Groq(api_key=self.api_key)
+
+    def call_gemma(self, question, responses):
+        print("groq gemma2 thinking...")
+        
+        completion = self.client.chat.completions.create(
+            model="gemma2-9b-it",
+            messages=[{"role": "user", "content": question}],
+            temperature=1,
+            max_tokens=1024,
+            top_p=1,
+            stream=False,
+            stop=None,
+        )
+        return completion #.choices[0].message.content.strip()
